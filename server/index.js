c36de025e6c349e3964a14cf5f1c081376676f4f
udaykiran <udaykiran@example.com>
2026-04-28 13:48:50 +0530

    "Fix_wildcard_route_for_Render"

--- server/index.js
+++ server/index.js
@@ -181,7 +181,7 @@ app.delete('/api/files/delete', async (req, res) => {
 });
 
 // All other GET requests not handled before will return the React app
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
